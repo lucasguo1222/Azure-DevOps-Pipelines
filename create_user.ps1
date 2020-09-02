ea7649027174cdc018045f9181d2a195ba4dad54
lucasguo1222 <70209481+lucasguo1222@users.noreply.github.com>
2020-09-02 15:51:05 +0800

    Update create_user.ps1

--- create_user.ps1
+++ create_user.ps1
@@ -1,16 +1,4 @@
-Write-Verbose “Get SID“
-$objId=”75b66b4c-c5e7-4fda-81f3-0e96426a8698“
-function ConvertTo-Sid {
-param (
-[string]$objectId
-)
-[guid]$guid = [System.Guid]::Parse($objectId)
-foreach ($byte in $guid.ToByteArray()) {
-$byteGuid += [System.String]::Format(“{0:X2}”, $byte)
-}
-return “0x” + $byteGuid
-}
-$SID=ConvertTo-Sid($objId)
+
 Write-Verbose “Create SQL connectionstring”
 $conn = New-Object System.Data.SqlClient.SQLConnection
 $conn.ConnectionString = “Server=v-luguo.database.windows.net;Initial Catalog=lucasDB;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30”
